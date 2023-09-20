bdd4add90c2442fbe251265cd4c2df382862b42f
Aleksandr Buganov <rivares89@gmail.com>
2023-09-20 23:44:27 +0300

    add title_str и docstring

--- str_func.py
+++ str_func.py
@@ -1,4 +1,9 @@
 def str_func(stroka):
+    '''Возвращает все буквы заглавными'''
     a = stroka.upper()
     return a
 
+def title_str(stroka):
+    """Возвращает заглавные первые буквы"""
+    b = stroka.title()
+    return b
